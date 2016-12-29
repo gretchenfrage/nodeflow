744636cb3d3de2dae11ca204c20461b7c34b3e72
Phoenix <kahlo.phoenix@gmail.com>
2016-12-29 00:14:30 -0600

    begin proxy package

---
+++ src/com/phoenixkahlo/nodenet/proxy/RuntimeDisconnectionException.java
@@ -0,0 +1,18 @@
+package com.phoenixkahlo.nodenet.proxy;
+
+public class RuntimeDisconnectionException extends RuntimeException {
+
+	private static final long serialVersionUID = 3492967446885152139L;
+
+	public RuntimeDisconnectionException() {}
+
+	public RuntimeDisconnectionException(String arg0) {
+		super(arg0);
+	}
+
+	public RuntimeDisconnectionException(Throwable arg0) {
+		super(arg0);
+	}
+
+
+}
