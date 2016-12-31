44c89ac34c3a400fc3842284c363be1e5a23f196
Phoenix <kahlo.phoenix@gmail.com>
2016-12-31 17:14:15 -0600

    reset proxy development

--- src/com/phoenixkahlo/nodenet/proxy/InvocationBuffer.java
+++
@@ -1,9 +0,0 @@
-package com.phoenixkahlo.nodenet.proxy;
-
-/**
- * A buffer for the invocations of a buffered reification of a proxy. The
- * contents can be flushed, which will cause the source to be invoked.
- */
-public class InvocationBuffer {
-
-}
